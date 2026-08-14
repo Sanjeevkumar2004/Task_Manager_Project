87f2be2e45a91aaa104f59f20a106502fb1e9a31
Sanjeevkumar M <130639043+Sanjeevkumar2004@users.noreply.github.com>
2026-08-14 08:17:34 +0530

    Add additional CORS allowed origin

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -167,7 +167,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://task-manager-project-ashen.vercel.app",
 ]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
